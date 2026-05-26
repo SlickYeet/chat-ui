3512644555a734b8015eb2c20d74cec7aa6376ad
SlickYeet <lasse@lx2.dev>
2026-05-26 03:48:06 -0700

    feat: refactor Textarea component to use TextareaAutosize for improved functionality

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,18 +1,26 @@
-import type * as React from "react"
+import React from "react"
+import TextareaAutosize, {
+  type TextareaAutosizeProps,
+} from "react-textarea-autosize"
 
 import { cn } from "@/lib/utils"
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
-  return (
-    <textarea
-      className={cn(
-        "field-sizing-content flex min-h-16 w-full resize-none rounded-none border border-transparent border-b-input bg-transparent px-0 py-3 text-base outline-none transition-[color,border-color] placeholder:text-muted-foreground focus-visible:border-b-ring disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-b-destructive md:text-sm dark:aria-invalid:border-b-destructive/50",
-        className,
-      )}
-      data-slot="textarea"
-      {...props}
-    />
-  )
-}
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <TextareaAutosize
+        className={cn(
+          "field-sizing-content flex min-h-16 w-full resize-none rounded-none border border-transparent border-b-input bg-transparent px-0 py-3 text-base outline-none transition-[color,border-color] placeholder:text-muted-foreground focus-visible:border-b-ring disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-b-destructive md:text-sm dark:aria-invalid:border-b-destructive/50",
+          className,
+        )}
+        data-slot="textarea"
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+
+Textarea.displayName = "Textarea"
 
 export { Textarea }
